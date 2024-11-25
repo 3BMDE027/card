fade8dd70fc0a42ae591038e328382b685ef1c2f
3BMDE027 <00001088474883sp@al.educacao.sp.gov.br>
2024-11-25 20:41:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'sigla para HyperText Markup Language, que em português significa Linguagem de Marcação de Hipertexto'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Escocia?',
+    'A capital da Escocia é Edimburgo'
+)
+
+criaCartao(
+    'Matematica',
+    'O que é uma função?',
+    'Uma função é uma relação de um conjunto com um conjunto'
+)
+
+criaCartao(
+    'ingles',
+    'Como se diz 10 em Inglês?',
+    '10 em ingles é Ten'
+)
